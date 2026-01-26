e7baf120042dd785fae47e9249ace570005d55f1
Poseidon-18 <tanmaybagri@gmail.com>
2026-01-26 21:40:52 +0530

    Add files via upload

---
+++ BasicStart.py
@@ -0,0 +1,24 @@
+def LibraryMS():
+    bookname= input("enter name of the book:")
+    bookauthor= input("enter book's author:")
+    datebought= input("enter date") #ya toh import date time ya fir int karke?
+    import mysql.connector 
+    connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1001001",
+            database="libraryms"
+        )
+if connection.is_connected():
+        print("Connected to MySQL database successfully!")
+else:
+      print("Failed to connect to MySQL database.")
+cur = database.cursor()
+
+# Use all the SQL you like
+cur.execute("INSERT INTO mylibrary(bookname,bookauthor) VALUES
+    ("Mahagatha"),("Farheniet 415")
+    ("Satyarath Nayak"),("Ray Bradbury"))"
+cur.execute("Select * from mylibrary")
+print("books updated!")
+database.close()
