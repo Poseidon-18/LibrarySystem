115f61ff8c2abd8785a31867848e3a33ecba1670
Poseidon-18 <tanmaybagri@gmail.com>
2026-01-27 09:59:49 +0530

    Update import mysql.py

--- import mysql.py
+++ import mysql.py
@@ -2,12 +2,13 @@ import mysql.connector
 connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="1001001",
-            database="libraryms"
+            password="ur password",
+            database="ur database"
         )
 if connection.is_connected():
         print("Connected to MySQL database successfully!")
 else:
       print("Failed to connect to MySQL database.")
+
 
     # iske baad cursor instance bana lo for things you need to do: cur=connection,cursor()
